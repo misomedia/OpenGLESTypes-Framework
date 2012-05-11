363842cbd1757cd445307c223031e30491b71282
Ryan Hiroaki Tsukamoto <ryantsukamoto@Ryan-Tsukamotos-iMac.local>
2012-05-11 16:31:22 -0700

    ...

---
+++ MisoOpenGLES/MisoOpenGLES/MisoOpenGLES.h
@@ -0,0 +1,13 @@
+//
+//  MisoOpenGLES.h
+//  MisoOpenGLES
+//
+//  Created by Ryan Tsukamoto on 5/4/12.
+//  Copyright (c) 2012 Miso Media. All rights reserved.
+//
+
+
+
+@interface MisoOpenGLES : NSObject
+
+@end
---
+++ MisoOpenGLES/MisoOpenGLES/MisoOpenGLES.m
@@ -0,0 +1,13 @@
+//
+//  MisoOpenGLES.m
+//  MisoOpenGLES
+//
+//  Created by Ryan Tsukamoto on 5/4/12.
+//  Copyright (c) 2012 Miso Media. All rights reserved.
+//
+
+#import "MisoOpenGLES.h"
+
+@implementation MisoOpenGLES
+
+@end
